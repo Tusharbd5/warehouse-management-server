a98dee1fd8f5519500b4fecb52658f971a1ea1db
Tusharbd5 <tushardebhata24@gmail.com>
2022-06-25 12:43:19 +0600

    data loaded from mongo to local server

--- index.js
+++ index.js
@@ -1,7 +1,37 @@
 const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
+// middleware
+app.use(cors());
+app.use(express.json());
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.flgqo61.mongodb.net/?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function run() {
+    try {
+        await client.connect();
+        const itemsCollection = client.db('warehouseDb').collection('item');
+
+        app.get('/product', async (req, res) => {
+            const query = {};
+            const cursor = itemsCollection.find(query);
+            const product = await cursor.toArray();
+            res.send(product);
+        });
+
+
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
 app.get('/', (req, res) => {
     res.send('Starting server')
 });
