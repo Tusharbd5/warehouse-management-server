887e91aa392a9511a3690d0e73d97f8797f69acd
Tusharbd5 <tushardebhata24@gmail.com>
2022-06-27 16:51:18 +0600

    JWT Server site work done

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
@@ -9,6 +10,21 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+function verifyJWT(req, res, next) {
+    const authHeader = req.headers.authorization;
+    if (!authHeader) {
+        return res.status(401).send({ message: 'unauthorized access' });
+    }
+    const token = authHeader.split(' ')[1];
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(403).send({ message: 'forbidden access' });
+        }
+        req.decoded = decoded;
+        next();
+    })
+
+}
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.flgqo61.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
@@ -18,6 +34,15 @@ async function run() {
         await client.connect();
         const itemsCollection = client.db('warehouseDb').collection('item');
         const userEntry = client.db('warehouseDb').collection('user_entry');
+
+        // Auth
+        app.post('/login', async (req, res) => {
+            const user = req.body;
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, {
+                expiresIn: '7d'
+            });
+            res.send({ accessToken });
+        });
 
         // Getting all data from database
         app.get('/product', async (req, res) => {
@@ -66,12 +91,18 @@ async function run() {
         });
 
         // User entry collection
-        app.get('/user-entry', async (req, res) => {
+        app.get('/user-entry', verifyJWT, async (req, res) => {
+            const decodedEmail = req.decoded.email;
             const email = req.query.email;
-            const query = { email: email };
-            const cursor = userEntry.find(query);
-            const entry = await cursor.toArray();
-            res.send(entry);
+            if (email === decodedEmail) {
+                const query = { email: email };
+                const cursor = userEntry.find(query);
+                const entry = await cursor.toArray();
+                res.send(entry);
+            }
+            else {
+                res.status(403).send({ message: 'forbidden access' });
+            }
         })
 
         app.post('/user-entry', async (req, res) => {
